24dcf213e79be25674a17e4656ce3e0e0210f20d
Sitare Arslantürk <60389066+sitarearsla@users.noreply.github.com>
2023-12-21 01:00:12 +0300

    add string catalog.

--- CardGameNetworking/CardGameNetworking/Views/Home/HomeViewModel.swift
+++ CardGameNetworking/CardGameNetworking/Views/Home/HomeViewModel.swift
@@ -6,3 +6,17 @@
 //
 
 import Foundation
+
+class HomeViewModel: ObservableObject {
+
+    @Published var player1ImageName = "back"
+    @Published var player2ImageName = "back"
+    @Published var result = "Welcome"
+    @Published var buttonLabel = "play"
+    @Published var infoPresented = false
+
+    init() {
+
+    }
+
+}

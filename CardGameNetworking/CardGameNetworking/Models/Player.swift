380d4f2fa9fec536d6514738a441d99e975b382f
Sitare Arslantürk <60389066+sitarearsla@users.noreply.github.com>
2024-01-15 05:14:00 +0300

    Update Player.swift
    Hashable

--- CardGameNetworking/CardGameNetworking/Models/Player.swift
+++ CardGameNetworking/CardGameNetworking/Models/Player.swift
@@ -8,7 +8,7 @@
 import Foundation
 
 // {"name":"Agumon","img":"https://digimon.shadowsmith.com/img/agumon.jpg","level":"Rookie"}
-struct Player: Codable{
+struct Player: Codable , Hashable {
 
     let name: String
     let img: String
